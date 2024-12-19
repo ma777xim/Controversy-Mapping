2c888f33d68f569892d8d30026cf30a6794cd7e6
Maxim Yurin <maxim.yurin@tum.de>
2024-12-19 15:00:49 +0100

    Add files via upload

--- register.js
+++ register.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
     import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
     import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+    import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,14 +21,30 @@
     const app = initializeApp(firebaseConfig);
     const analytics = getAnalytics(app);
 
-    // inputs
-    const email = document.getElementById('email').value;
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+
 
     // submit
     const submit = document.getElementById('submit');
     submit.addEventListener("click",function(event){
+        // inputs
+        const email = document.getElementById('email').value;
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
     event.preventDefault()
-    alert (5)
+
+
+
+    const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        alert(Mhm nice its working)
     })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(Mhm nice it worked)
+    });
+})
