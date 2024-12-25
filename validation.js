5e2e577925550431288045421322c41f0f6fabea
Maxim Yurin <maxim.yurin@tum.de>
2024-12-25 23:35:29 +0100

    Add files via upload

--- validation.js
+++ validation.js
@@ -50,24 +50,20 @@ allInputs.forEach(input=>{
    })
 })
 
-
-
 const signUpButton=document.getElementById('signUpButton')
 
 const signInButton=document.getElementById('signInButton')
 
 const signInForm=document.getElementById('signIn')
 
-const signUpForm=document.getElementById('signIn')
+const signUpForm=document.getElementById('signUp')
 
-signUpButton.addEventListener('click',function{
-    signInForm.style.display="none";
-    signUpForm.style.display="block";
+signUpButton.addEventListener('click', function() {
+    signInForm.style.display = "none";
+    signUpForm.style.display = "block";
+});
 
-})
-
-signInButton.addEventListener('click',function{
-    signInForm.style.display="block";
-    signUpForm.style.display="none";
-
-})
+signInButton.addEventListener('click', function() {
+    signInForm.style.display = "block";
+    signUpForm.style.display = "none";
+});
